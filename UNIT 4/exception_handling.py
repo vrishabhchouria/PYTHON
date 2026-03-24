b40fd2fd9fa8029b658249d1670a86e822e4d279
vrishabhchouria <tlwvrishabh25@gmail.com>
2026-03-24 22:24:04 +0530

    Delete UNIT 4 directory

--- UNIT 4/exception_handling.py
+++
@@ -1,7 +0,0 @@
-try:
-    f = open(input(), "r")
-    print(f.read())
-except FileNotFoundError:
-    print("File not found")
-except PermissionError:
-    print("No permission")
--- UNIT 4/json_to_csv.py
+++
@@ -1,9 +0,0 @@
-import json, csv
-
-with open("data.json") as f:
-    data = json.load(f)
-
-with open("data.csv", "w", newline="") as f:
-    writer = csv.DictWriter(f, fieldnames=data[0].keys())
-    writer.writeheader()
-    writer.writerows(data)

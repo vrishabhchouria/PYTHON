720ed6dc7b1887e1013c651b00407053cf4e2ff9
vrishabhchouria <tlwvrishabh25@gmail.com>
2026-03-24 20:19:28 +0530

    Add files via upload

---
+++ UNIT1/experiment1.py
@@ -0,0 +1,16 @@
+fruits = ["apple", "banana", "cherry", "mango"]
+print(fruits)
+
+print(fruits[1])
+
+fruits.append("strawberry")
+print(fruits)
+
+fruits.remove("cherry")
+print(fruits)
+
+fruits.sort()
+print(fruits)
+
+fruits.reverse()
+print(fruits)
